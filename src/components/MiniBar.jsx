bb67efeb92d58f772bd9c065a4c5d1dce8cadcf7
Amrita Gupta <amamrita0818@gmail.com>
2024-07-07 02:04:08 +0530

    update styling

--- src/components/MiniBar.jsx
+++ src/components/MiniBar.jsx
@@ -5,10 +5,10 @@ const MiniBar = () => {
     <div className='w-full '>
         <div className='w-[50%] h-full  mx-auto max-w-[1200px] pt-16 flex items-center justify-center'>
             <ul className='flex flex-row gap-20 bg-[#EEF5FF] rounded-full px-16 py-4'>
-            <li className='text-[#1A73E8] font-[400] leading-[22px] text-center text-[25px]'>Refer</li>
-            <li className='text-[#4B4B4B] font-[400] leading-[22px] text-center text-[25px]'>Benefits</li>
-            <li className='text-[#4B4B4B] font-[400] leading-[22px] text-center text-[25px]'>FAQs</li>
-            <li className='text-[#4B4B4B] font-[400] leading-[22px] text-center text-[25px]'>Support</li>
+            <li className='text-[#1A73E8] font-[400] leading-[22px] text-center text-[20px]'>Refer</li>
+            <li className='text-[#4B4B4B] font-[400] leading-[22px] text-center text-[20px]'>Benefits</li>
+            <li className='text-[#4B4B4B] font-[400] leading-[22px] text-center text-[20px]'>FAQs</li>
+            <li className='text-[#4B4B4B] font-[400] leading-[22px] text-center text-[20px]'>Support</li>
             </ul>
         </div>
     </div>
